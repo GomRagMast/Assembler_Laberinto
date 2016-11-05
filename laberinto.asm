eaa25daa559333cf0d3165454c28bc5f061fd3dd
Rafael <gomezrafaelantonio@gmail.com>
2016-11-04 22:26:32 -0500

    Agrega pixeles aleatorios para puntuacion

--- laberinto.asm
+++ laberinto.asm
@@ -1,15 +1,16 @@
-
 org 100h
 
 mov ah, 0
 mov al, 13h
 int 10h    
 
-lea bx, lab
+lea bx, lab 
+call aleatorio
 
 inicio:
     cmp [bx],'x'
-    je et1
+    je et1 
+    call punto
     jmp et2
 
 et1:  
@@ -28,9 +29,22 @@ et4:
     cmp fila, 50
     je fin
     jmp inicio
-fin: 
+fin:  
     ;Codigo robot
 
+ret  
+
+punto:
+    mov al, valor
+    cmp pin,al
+    jge reload  
+    inc pin
+ret    
+reload:  
+    mov color,40         
+    call pixel
+    mov pin,0 
+    call aleatorio
 ret    
 
 pixel:
@@ -40,13 +54,24 @@ pixel:
     mov ah, 0ch
     int 10h
     
+ret              
+;------------------------------------------------------
+aleatorio:
+    mov ah,0
+    int 1ah
+    mov ax, dx
+    xor dx, dx
+    mov cx, 100
+    div cx
+    mov valor, dl
 ret
-
+;--------------------------------------------
+;Variables
 columna dw 10
 fila    dw 10
-color   db 40    
+color   db 40                   
+pin     db 0 
+valor   db 0
 
-;------------------------------------------------------
 ;Importar Libreria mapa laberinto
 include "_laberinto.map.asm"    
-    
